29688496f1c9e104a1c419f6c44ccbcd11244162
Govind Naag <156845701+Govind44naag@users.noreply.github.com>
2025-05-04 23:43:09 +0530

    Create addLinkedList.java

---
+++ ds-algo/addLinkedList.java
@@ -0,0 +1,28 @@
+class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) { this.data = data; }
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+}
